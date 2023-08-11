b06a71fd012510971e579065e0348356bc73f476
smyoo-pb <sm.yoo@precision-bio.com>
2023-08-11 09:12:21 +0900

    feat: section4 end

--- learn-spring-fraework-02/src/main/java/com/precisionbio/learnspringframework/example/c1/MongoDbDataService.java
+++ learn-spring-fraework-02/src/main/java/com/precisionbio/learnspringframework/example/c1/MongoDbDataService.java
@@ -2,12 +2,17 @@ package com.precisionbio.learnspringframework.example.c1;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 /**
  * Component 어노테이션으로 Spring Bean 등록
  * Primary 어노테이션으로 우선순위 설정
  */
-@Component
+// @Component
+/**
+ * Repository database와 연결한다는 것을 명시
+ */
+@Repository
 @Primary
 public class MongoDbDataService implements DataService {
 
--- learn-spring-fraework-02/src/main/java/com/precisionbio/learnspringframework/example/c1/MySQLDataService.java
+++ learn-spring-fraework-02/src/main/java/com/precisionbio/learnspringframework/example/c1/MySQLDataService.java
@@ -1,11 +1,13 @@
 package com.precisionbio.learnspringframework.example.c1;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 /**
  * Component 어노테이션으로 Spring Bean 등록
  */
-@Component
+// @Component
+@Repository
 public class MySQLDataService implements DataService {
 
     @Override
